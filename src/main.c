f8723e7782a86b3618bcbd04a37e8675f2ab0322
dfall33 <david.c.fall33@gmail.com>
2024-10-29 19:51:36 -0400

    initial commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/**
+ * @file main.c
+ * @authors David Fall, Jennifer Ferguson, Luke Lenz, Eduard Tanase
+ * @date November 2024
+ * @brief ECE 36200 Group Project, Fall 2024
+ */
+
+#include "stm32f0xx.h"
+
+int main()
+{
+    return 0;
+}
